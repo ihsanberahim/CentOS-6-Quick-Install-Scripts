be247db20b13f091177f167f8de43e19006ba3fd
Muhammad Nur'Ihsan BIn Berahim <ihsanberahim@gmail.com>
2016-04-29 00:10:32 +0800

    Update install-mailcatcher.sh

--- install-mailcatcher.sh
+++ install-mailcatcher.sh
@@ -5,4 +5,5 @@ echo "MailCatcher: https://mailcatcher.me/"
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /usr/local/rvm/scripts/rvm
 gem install mailcatcher
