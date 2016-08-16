4b33468fb4bd0c4d87bea873dad85f72247cb5fb
Muhammad Nur'Ihsan Bin Berahim <ihsanberahim@gmail.com>
2016-08-16 20:38:13 +0800

    Update installLAMP.sh

--- installLAMP.sh
+++ installLAMP.sh
@@ -12,8 +12,11 @@ rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
 yum update -y
 yum upgrade -y
 
-yum install -y httpd php56w mysql55w mysql55w-server
-yum install -y php56w-intl php56w-mcrypt php56w-mbstring php56w-xml php56w-soap php56w-gd
+yum install -y yum-plugin-replace
+yum install -y httpd
+yum install -y php56w php56w-intl php56w-mcrypt php56w-mbstring php56w-xml php56w-soap php56w-gd php56w-pdo php56w-pdo_mysql
+yum replace -y mysql --replace-with mysql55w
+yum install -y mysql55w mysql55w-server
 
 
 chkconfig mysqld on
