d42cab0ad873b0640777dc262a426756fdb50d71
Muhammad Nur'Ihsan Bin Berahim <ihsanberahim@gmail.com>
2016-11-26 22:32:48 +0800

    Update installLAMP.sh

--- installLAMP.sh
+++ installLAMP.sh
@@ -4,9 +4,8 @@ clear
 
 echo 'Going to install the LAMP stack on your machine, here we go...'
 echo '------------------------'
-read -p "MySQL Password: " mysqlPassword
-read -p "Retype password: " mysqlPasswordRetype
 
+yum install epel-release
 rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
 
 yum update -y
@@ -14,12 +13,13 @@ yum upgrade -y
 
 yum install -y yum-plugin-replace
 yum install -y httpd
+yum install libmcrypt --enablerepo=epel
 yum install -y php56w php56w-intl php56w-mcrypt php56w-mbstring php56w-xml php56w-soap php56w-gd php56w-pdo php56w-pdo_mysql
 yum replace -y mysql --replace-with mysql55w
+yum replace -y mysql-libs --replace-with mysql55w-libs
 yum install -y mysql55w mysql55w-server
 
-
-chkconfig mysqld on
-chkconfig httpd on
-
-/usr/bin/mysql_secure_installation
+echo 'Wait. Run these 3 lines'
+echo '/usr/bin/mysql_secure_installation'
+echo 'chkconfig mysqld on'
+echo 'chkconfig httpd on'
